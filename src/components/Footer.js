6a46bd707a10a9297b6ffde3f9e0aaa294f4251e
lerritx <103357746+lerritx@users.noreply.github.com>
2024-12-22 22:27:16 +0700

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+"use client";
+export default function Footer() {
+return (
+<footer className="bg-purple-500 text-white py-2 px-4 w-full shadow-md text-center fixed
+
+bottom-0">
+
+<p className="text-lg font-bold">Lerrit Hospital</p>
+<p className="text-sm">123 Health St, Wellness City, HC 12345</p>
+<p className="text-sm">©2024 BSC Hospital. All rights reserved.</p>
+</footer>
+);
+}
